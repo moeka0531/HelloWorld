ff62a595abd71a8ef297317f1cb428e049f06dd2
g13933nm <g13933nm@s30428.tsuda.ac.jp>
2015-04-10 11:06:13 +0900

    HelloWorldを出力するプログラムです プロジェクト名:first

---
+++ src/first/First.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first;
+
+/**
+ *
+ * @author g13933nm
+ */
+public class First {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+    }
+}
